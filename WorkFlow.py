696657cfff160c9a7a6ae6ea3902fdfaf19b1fa9
Daniel Wray <daniel_wray@hotmail.com>
2022-02-25 10:04:41 +0000

    Add files via upload

---
+++ WorkFlow.py
@@ -0,0 +1,13 @@
+# Workflow.py
+import webbrowser
+
+#The open.new function will open the first webpage in the default web browser
+webbrowser.open_new("https://www.google.co.uk")
+
+#The open_new_tab function will open the following webpage in the already open default browser
+webbrowser.open_new_tab("https://www.facebook.com")
+
+webbrowser.open_new_tab("https://www.github.com")
+
+#os.system can be used to open .exe applications. 
+os.system("start outlook")
